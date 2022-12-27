d6f0a24a461ae9f41d9bd3679b871bcac6a7ba75
liran0427 <liran0427@gmail.com>
2022-12-27 13:28:14 -0700

    create INITIAL_MEMOS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const INITIAL_MEMOS = [
+  {
+    id: "a1",
+    date: new Date(2020, 1, 1),
+    title: "Task 1",
+    completion: "To Do",
+  },
+  {
+    id: "a2",
+    date: new Date(2020, 2, 1),
+    title: "Task 2",
+    completion: "Finished",
+  },
+  {
+    id: "a3",
+    date: new Date(2020, 3, 1),
+    title: "Task 3",
+    completion: "To Do",
+  },
+];
+
 function App() {
-  return <div>hh</div>;
+  return <div>{INITIAL_MEMOS[0].id}</div>;
 }
 
 export default App;
